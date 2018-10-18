83204ac3087852cb98f9b24aeca4c57918354ffe
Gimena_UNICAH <Gimena_UNICAH@GIMENA>
2018-10-17 19:47:41 -0600

    Creando el formulario de eliminar departamentos

---
+++ Adventure-Departamentos/frmEliminar.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Adventure_Departamentos
+{
+    partial class frmEliminar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbEliminar = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbEliminar
+            // 
+            this.lbEliminar.FormattingEnabled = true;
+            this.lbEliminar.Items.AddRange(new object[] {
+            "        [INFORMACION DE LOS DEPARTAMENTOS]"});
+            this.lbEliminar.Location = new System.Drawing.Point(12, 12);
+            this.lbEliminar.Name = "lbEliminar";
+            this.lbEliminar.Size = new System.Drawing.Size(297, 186);
+            this.lbEliminar.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "btnEliminar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "btnRegresar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmEliminar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 236);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbEliminar);
+            this.Name = "frmEliminar";
+            this.Text = "frmEliminar";
+            this.Load += new System.EventHandler(this.frmEliminar_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbEliminar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Adventure-Departamentos/frmEliminar.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace Adventure_Departamentos
+{
+    public partial class frmEliminar : Form
+    {
+        //creando la conexion, antes de todo porque cargare los datos desde el load
+        SqlConnection conn = new SqlConnection(@"server = (local)\sqlexpress;
+                           integrated security = true; database = AdventureWorks2014;");
+
+        public frmEliminar()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // creamos el comando para ejecutar el StoreProcedure.
+            SqlCommand cmd = new SqlCommand("sp_EliminarDepartamento", conn);
+
+            // especificamos el tipo de comando
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                // especificamos los parametros del procemiento almacenado.
+                cmd.Parameters.Add(new SqlParameter("@Codigo", SqlDbType.SmallInt));
+                cmd.Parameters["@Codigo"].Value = lbEliminar.SelectedItem.ToString();
+
+                // abrimos las conexión.
+                conn.Open();
+
+                // Eliminamos el departamento.
+
+                if (lbEliminar.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Por favor debe seleccionar un departamento", "Error en ingreso", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    // ejecutamos el comando
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Departamento eliminado correctamente", "Control de departamentos", MessageBoxButtons.OK);
+                    lbEliminar.SelectedIndex = -1;
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message + ex.StackTrace, "Detalles de la exepción");
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Recargue el valor de los departamentos" + ex.StackTrace, "Detalles de la excepción");
+            }
+            finally
+            {
+                // cerramos la conexión.
+                conn.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmEliminar_Load(object sender, EventArgs e)
+        {
+            // Creamos el query de selección de la tabla departamontos
+            string sql1 = "Select DepartmentID,Name FROM HumanResources.Department";
+
+            // Creamos el comando
+            SqlCommand cmd = new SqlCommand(sql1, conn);
+
+            // establecemos la conexión y llenado el dataGridView
+            try
+            {
+                // abrimos la conexión.
+                conn.Open();
+
+                // Ejecutamos el query via un DataReader y llenamo el Grid.
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                // llenamos el DataGrid
+                while (rdr.Read())
+                {
+                    lbEliminar.Items.Add(rdr[0]);
+                    lbEliminar.Items.Add(rdr[1]);
+
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message + ex.StackTrace + "Detalles de la exepción");
+
+            }
+            finally
+            {
+                // Cerramos la conexión.
+                conn.Close();
+            }
+        }
+    }
+}
